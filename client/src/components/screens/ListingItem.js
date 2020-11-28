aafad510ed5468684bc2f6d99a7aec4d627f8062
SamKlep <shamdemann23@gmail.com>
2020-11-28 10:28:51 -0600

    Styling on ListingItem component.

--- client/src/components/screens/ListingItem.js
+++ client/src/components/screens/ListingItem.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Container } from 'react-bootstrap'
 
 const ListingItem = ({ match }) => {
   const [data, setData] = useState({})
@@ -20,31 +20,37 @@ const ListingItem = ({ match }) => {
 
   return (
     <>
-      <Link className='btn btn-light my-3' to='/'>
-        Go Back
-      </Link>
-      <Row className='justify-content-md-center  mb-5'>
-        <Col md={6}>
-          <Image src={data.photo_main} alt={data.title} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <strong>
-                <h3>{data.title}</h3>
-              </strong>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>
-                <h5>{data.price}</h5>
-              </strong>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h6>{data.description}</h6>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col>
+            <Link className='btn btn-dark my-3' to='/'>
+              Go Back
+            </Link>
+            <Row className='justify-content-md-center  mb-5'>
+              <Col md={6}>
+                <Image src={data.photo_main} alt={data.title} fluid />
+              </Col>
+              <Col md={3}>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <strong>
+                      <h3>{data.title}</h3>
+                    </strong>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <strong>
+                      <h5>{data.price}</h5>
+                    </strong>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <h6>{data.description}</h6>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
